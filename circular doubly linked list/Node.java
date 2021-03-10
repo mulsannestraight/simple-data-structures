382f92741349db5de5367622b75bdeccd5515c2c
mulsannestraight <22200664+mulsannestraight@users.noreply.github.com>
2021-03-09 21:56:47 -0500

    Create Node.java

---
+++ circular doubly linked list/Node.java
@@ -0,0 +1,31 @@
+public class Node {
+  private int data;
+  private Node next;
+  private Node prev;
+  
+  public Node(int data, Node next, Node prev) {
+    this.data = data;
+    this.next = next;
+    this.prev = prev;
+  }
+  public Node(int data) {
+    this.data = data;
+    this.next = null;
+    this.prev = null;
+  }
+  public Node getNext() {
+    return next;
+  }
+  public void setNext(Node next) {
+    this.next = next;
+  }
+  public Node getPrev() {
+    return prev;
+  }
+  public void setPrev(Node prev) {
+    this.prev = prev;
+  }
+  public int getData() {
+    return data;
+  }
+}
